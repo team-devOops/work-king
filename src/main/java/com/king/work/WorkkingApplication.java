765011edd5abe4650580154391939ba11234d8ab
zinzoddari <zinzoddari@gmail.com>
2023-06-27 22:31:38 +0900

    init commit

---
+++ src/main/java/com/king/work/WorkkingApplication.java
@@ -0,0 +1,13 @@
+package com.king.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkkingApplication.class, args);
+	}
+
+}
